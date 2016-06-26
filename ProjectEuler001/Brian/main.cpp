e2c711d2df671cc5dea11739791949f8fc3f1595
Brian Ristau <13ristaub@gmail.com>
2016-06-26 15:45:19 -0400

    revised contents of question 1

--- ProjectEuler001/Brian/main.cpp
+++ ProjectEuler001/Brian/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
 	// loop through all the numbers from 0 to 999
 	for (int iter=0; iter<1000; iter++) {
 		// add the current number if it is evenly divisible
-		total += !(iter%3) || !(iter%5) ? iter : 0;
+		if (iter % 3 == 0 || iter % 5 == 0) total += iter;
 	}
 
 	// output the result
