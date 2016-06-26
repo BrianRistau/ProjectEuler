b3696292182fc03e129a14f64e45593044fe6703
Brian Ristau <13ristaub@gmail.com>
2016-06-26 16:16:53 -0400

    question 7 - brian

--- ProjectEuler007/Brian/main.cpp
+++ ProjectEuler007/Brian/main.cpp
@@ -6,9 +6,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// function prototypes
+int nextPrime(int current);
+int isPrime(int num);
+
 // program entry point
 int main(int argc, char **argv) {
 
+	// keep track of the current prime and its index
+	int num = 2, index = 1;
+	while (index < 10001) {
+		// keep calculating the next prime number
+		num = nextPrime(num);
+		index += 1;
+	}
 
+	// output the answer
+	printf("the answer is %d.\n", num);
+
+}
+
+// function to get the next prime from the specified starting point
+int nextPrime(int current) {
+
+	// create an interation variable
+	int iter = current + 1;
+	// loop endlessly while a prime is being found
+	while (1) {
+		if (isPrime(iter)) return iter;
+		else iter += 1;
+	}
+
+}
+
+// function to return whether or not the given number is prime
+int isPrime(int num) {
+
+	// check the base cases
+	if (num == 2 || num == 3) return 1;
+	if (num % 2 == 0) return 0;
+	if (num % 3 == 0) return 0;
+
+	// iterate up to the number's square root, checking for factors
+	for (int iter = 5; iter * iter <= num; iter += 2) {
+		if (num % iter == 0) return 0;
+	}
+
+	// return true if no factors were found
+	return 1;
 
 }
